88ed3ea947cb21e4f5521a6c1b81116d906cd117
Fran Martin Rivas <fmartinrivas2@gmail.com>
2022-03-18 09:49:35 +0100

    feat(openmaptiles): ctxmaps conf

--- layers/aerodrome_label/aerodrome_label.sql
+++ layers/aerodrome_label/aerodrome_label.sql
@@ -20,54 +20,9 @@ CREATE OR REPLACE FUNCTION layer_aerodrome_label(bbox geometry,
             )
 AS
 $$
-SELECT
-    -- etldoc: osm_aerodrome_label_point -> layer_aerodrome_label:z8
-    -- etldoc: osm_aerodrome_label_point -> layer_aerodrome_label:z9
-    ABS(osm_id) AS id, -- mvt feature IDs can't be negative
-    NULL::int as icgc_id,
-    oalp.geometry,
-    oalp.name,
-    COALESCE(NULLIF(name_en, ''), name) AS name_en,
-    COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
-    oalp.tags,
-    aerodrome_type AS class,
-    NULLIF(iata, '') AS iata,
-    NULLIF(icao, '') AS icao,
-    substring(ele FROM E'^(-?\\d+)(\\D|$)')::int AS ele,
-    round(substring(ele FROM E'^(-?\\d+)(\\D|$)')::int * 3.2808399)::int AS ele_ft
-FROM osm_aerodrome_label_point oalp, icgc_data.catalunya c
-WHERE oalp.geometry && bbox
-  AND ST_Disjoint(c.geometry, oalp.geometry)
-  AND aerodrome_type = 'international'
-  AND iata <> ''
-  AND zoom_level BETWEEN 8 AND 9
-
-UNION ALL
-
-SELECT
-    -- etldoc: osm_aerodrome_label_point -> layer_aerodrome_label:z10_
-    ABS(osm_id) AS id, -- mvt feature IDs can't be negative
-    NULL::int as icgc_id,
-    oalp.geometry,
-    oalp.name,
-    COALESCE(NULLIF(name_en, ''), name) AS name_en,
-    COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
-    oalp.tags,
-    aerodrome_type AS class,
-    NULLIF(iata, '') AS iata,
-    NULLIF(icao, '') AS icao,
-    substring(ele FROM E'^(-?\\d+)(\\D|$)')::int AS ele,
-    round(substring(ele FROM E'^(-?\\d+)(\\D|$)')::int * 3.2808399)::int AS ele_ft
-FROM osm_aerodrome_label_point oalp, icgc_data.catalunya c
-WHERE oalp.geometry && bbox
-  AND ST_Disjoint(c.geometry, oalp.geometry)
-  AND zoom_level BETWEEN 10 AND 14
-
-UNION ALL
-
 SELECT 
     -- icgc aerodrome label
-    NULL::int AS id,
+    NULL::bigint AS id,
     icgc_id,
     geom,
     name,
--- layers/aeroway/aeroway.sql
+++ layers/aeroway/aeroway.sql
@@ -12,77 +12,15 @@ CREATE OR REPLACE FUNCTION layer_aeroway(bbox geometry, zoom_level int)
             )
 AS
 $$
-SELECT icgc_id, geometry, aeroway AS class, ref, tipus
+SELECT icgc_id, geom, aeroway AS class, ref, tipus
 FROM (
-         -- etldoc:  osm_aeroway_linestring_gen_z10 -> layer_aeroway:z10
-         SELECT NULL::int AS icgc_id, oel.geometry, oel.aeroway, oel.ref, NULL::int as tipus
-         FROM osm_aeroway_linestring_gen_z10 oel, icgc_data.catalunya c
-         WHERE zoom_level = 10
-            AND ST_Disjoint(c.geometry, oel.geometry)
-         UNION ALL
-         -- etldoc:  osm_aeroway_linestring_gen_z11 -> layer_aeroway:z11
-         SELECT NULL::int AS icgc_id, oel.geometry, oel.aeroway, oel.ref, NULL::int as tipus
-         FROM osm_aeroway_linestring_gen_z11 oel, icgc_data.catalunya c
-         WHERE zoom_level = 11
-            AND ST_Disjoint(c.geometry, oel.geometry)
-         UNION ALL
-         -- etldoc:  osm_aeroway_linestring_gen_z12 -> layer_aeroway:z12
-         SELECT NULL::int AS icgc_id, oel.geometry, oel.aeroway, oel.ref, NULL::int as tipus
-         FROM osm_aeroway_linestring_gen_z12 oel, icgc_data.catalunya c
-         WHERE zoom_level = 12
-            AND ST_Disjoint(c.geometry, oel.geometry)
-         UNION ALL
-         -- etldoc:  osm_aeroway_linestring -> layer_aeroway:z13
-         -- etldoc:  osm_aeroway_linestring -> layer_aeroway:z14_
-         SELECT NULL::int AS icgc_id, oel.geometry, oel.aeroway, oel.ref, NULL::int as tipus
-         FROM osm_aeroway_linestring oel, icgc_data.catalunya c
-         WHERE zoom_level >= 13
-            AND ST_Disjoint(c.geometry, oel.geometry)
-         UNION ALL
-         -- etldoc:  osm_aeroway_polygon_gen_z10 -> layer_aeroway:z10
-         SELECT NULL::int AS icgc_id, oep.geometry, oep.aeroway, oep.ref, NULL::int as tipus
-         FROM osm_aeroway_polygon_gen_z10 oep, icgc_data.catalunya c
-         WHERE zoom_level = 10
-            AND ST_Disjoint(c.geometry, oep.geometry)
-         UNION ALL
-         -- etldoc:  osm_aeroway_polygon_gen_z11 -> layer_aeroway:z11
-         SELECT NULL::int AS icgc_id, oep.geometry, oep.aeroway, oep.ref, NULL::int as tipus
-         FROM osm_aeroway_polygon_gen_z11 oep, icgc_data.catalunya c
-         WHERE zoom_level = 11
-            AND ST_Disjoint(c.geometry, oep.geometry)
-         UNION ALL
-         -- etldoc:  osm_aeroway_polygon_gen_z12 -> layer_aeroway:z12
-         SELECT NULL::int AS icgc_id, oep.geometry, oep.aeroway, oep.ref, NULL::int as tipus
-         FROM osm_aeroway_polygon_gen_z12 oep, icgc_data.catalunya c
-         WHERE zoom_level = 12
-            AND ST_Disjoint(c.geometry, oep.geometry)
-         UNION ALL
-         -- etldoc:  osm_aeroway_polygon_gen_z13 -> layer_aeroway:z13
-         SELECT NULL::int AS icgc_id, oep.geometry, oep.aeroway, oep.ref, NULL::int as tipus
-         FROM osm_aeroway_polygon_gen_z13 oep, icgc_data.catalunya c
-         WHERE zoom_level = 13
-             AND ST_Disjoint(c.geometry, oep.geometry)                       
-         UNION ALL
-         -- etldoc:  osm_aeroway_polygon -> layer_aeroway:z14_
-         SELECT NULL::int AS icgc_id, oep.geometry, oep.aeroway, oep.ref, NULL::int as tipus
-         FROM osm_aeroway_polygon oep, icgc_data.catalunya c
-         WHERE zoom_level = 14
-            AND ST_Disjoint(c.geometry, oep.geometry)
-         UNION ALL
-         
-         -- etldoc:  osm_aeroway_point -> layer_aeroway:z14_
-         SELECT NULL::int AS icgc_id, oep.geometry, oep.aeroway, oep.ref, NULL::int as tipus
-         FROM osm_aeroway_point oep, icgc_data.catalunya c
-         WHERE zoom_level = 14
-            AND ST_Disjoint(c.geometry, oep.geometry)
-         UNION ALL
 
          -- icgc aeroway
          SELECT icgc_id, geom, class AS aeroway, NULL::text AS ref, tipus
          FROM icgc_data.aeroway
          WHERE zoom_level >= 10 AND geom && bbox
      ) AS zoom_levels
-WHERE geometry && bbox;
+WHERE geom && bbox;
 $$ LANGUAGE SQL STABLE
                 -- STRICT
                 PARALLEL SAFE;

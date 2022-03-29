e336fa138ae5bc1cdfa28352eaa88fb0ae96467c
Fran Martin Rivas <fmartinrivas2@gmail.com>
2022-03-29 13:25:07 +0200

    feat(water-name): add water_name layer

--- layers/water_name/water_name.sql
+++ layers/water_name/water_name.sql
@@ -8,15 +8,12 @@ CREATE OR REPLACE FUNCTION layer_water_name(bbox geometry, zoom_level integer)
                 icgc_id      bigint,
                 geometry     geometry,
                 name         text,
-                name_en      text,
-                name_de      text,
-                tags         hstore,
+                "name:latin" text,
                 class        text,
-                intermittent int,
-                zoom         int,
+                icgc_zoom    int,
                 rank         int,
-                codigeo     int,
-                fontsize    float
+                codigeo      int,
+                fontsize     float
             )
 AS
 $$
@@ -30,12 +27,9 @@ SELECT
     NULL::INT AS icgc_id,
     w.geometry,
     w.name,
-    COALESCE(NULLIF(name_en, ''), name) AS name_en,
-    COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
-    w.tags,
+    w.name AS "name:latin",
     'lake'::text AS class,
-    is_intermittent::int AS intermittent,
-    NULL::INT AS zoom,
+    NULL::INT AS icgc_zoom,
     NULL::INT AS rank,
     NULL::INT AS codigeo,
     NULL::FLOAT AS fontsize
@@ -56,12 +50,9 @@ SELECT
     NULL::INT AS icgc_id,
     w.geometry,
     w.name,
-    COALESCE(NULLIF(name_en, ''), name) AS name_en,
-    COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
-    w.tags,
+    w.name AS "name:latin",
     'lake'::text AS class,
-    is_intermittent::int AS intermittent,
-    NULL::INT AS zoom,
+    NULL::INT AS icgc_zoom,
     NULL::INT AS rank,
     NULL::INT AS codigeo,
     NULL::FLOAT AS fontsize
@@ -80,20 +71,16 @@ SELECT
     -- etldoc: osm_marine_point ->  layer_water_name:z14_
     osm_id * 10 AS osm_id_hash,
     NULL::INT AS icgc_id,
-    omp.geometry,
-    omp.name,
-    COALESCE(NULLIF(name_en, ''), name) AS name_en,
-    COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
-    omp.tags,
+    geometry,
+    name,
+    name AS "name:latin",
     place::text AS class,
-    is_intermittent::int AS intermittent,
-    NULL::INT AS zoom,
+    NULL::INT AS icgc_zoom,
     NULL::INT AS rank,
     NULL::INT AS codigeo,
     NULL::FLOAT AS fontsize
-FROM osm_marine_point omp, ocean_d15 od
-WHERE omp.geometry && bbox
-  AND ST_Disjoint(od.geom, omp.geometry)
+FROM osm_marine_point 
+WHERE geometry && bbox
   AND (
         place = 'ocean'
         OR (zoom_level >= "rank" AND "rank" IS NOT NULL)
@@ -107,12 +94,9 @@ SELECT
      w.icgc_id,
      w.geom,
      w.name,
-     NULL::TEXT AS name_en,
-     NULL::TEXT AS name_de,
-     NULL::hstore AS tags,
+     name AS "name:latin",
      w.class,
-     NULL::INT AS intermittent,
-     w.zoom,
+     w.zoom AS icgc_zoom,
      w.rank,
      w.codigeo,
      w.fontsize
@@ -126,12 +110,9 @@ SELECT
      w.icgc_id,
      w.geom,
      w.name,
-     NULL::TEXT AS name_en,
-     NULL::TEXT AS name_de,
-     NULL::hstore AS tags,
+     name AS "name:latin",
      w.class,
-     NULL::INT AS intermittent,
-     w.zoom,
+     w.zoom AS icgc_zoom,
      w.rank,
      w.codigeo,
      w.fontsize

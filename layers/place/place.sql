b79e850cb12a55155fbccc1fba0effe30bb68681
Fran Martin Rivas <fmartinrivas2@gmail.com>
2022-04-01 12:43:30 +0200

    feat(place): add name:latin to place

--- layers/place/place.sql
+++ layers/place/place.sql
@@ -1,11 +1,13 @@
 -- etldoc: layer_place[shape=record fillcolor=lightpink, style="rounded,filled",
 -- etldoc:     label="layer_place | <z0_3> z0-3|<z4_7> z4-7|<z8_11> z8-11| <z12_14> z12-z14+" ] ;
+drop function if exists layer_place(bbox geometry, zoom_level int, pixel_width numeric);
 CREATE OR REPLACE FUNCTION layer_place(bbox geometry, zoom_level int, pixel_width numeric)
     RETURNS TABLE
             (
                 icgc_id        int,
                 geometry       geometry,
                 name           text,
+                "name:latin"   text,
                 class          text,
                 "rank"         bigint
             )
@@ -16,6 +18,7 @@ SELECT
     icgc_id,
     geometry,
     name,
+    name AS "name:latin",
     class,
     rank
 FROM icgc_data.place_div_admin

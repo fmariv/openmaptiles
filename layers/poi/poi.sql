0770baaf47ae520dd9f60e57f6238de13014ee22
Fran Martin <francisco.martin@icgc.cat>
2022-05-10 12:50:02 +0000

    feat(poi): add stop_bus layer

--- layers/poi/poi.sql
+++ layers/poi/poi.sql
@@ -4,21 +4,23 @@ DROP FUNCTION IF EXISTS layer_poi(bbox geometry, zoom_level integer, pixel_width
 CREATE OR REPLACE FUNCTION layer_poi(bbox geometry, zoom_level integer, pixel_width numeric)
     RETURNS TABLE
             (
-                icgc_id        int,
-                geometry       geometry,
-                name           text,
-                "name:latin"   text,
-                class          text,
-                subclass       text,
-                categoria      text,
-                layer          text,
-                "rank"         int,
-                icgc_zoom      int
+                icgc_id             int,
+                geometry            geometry,
+                name                text,
+                "name:latin"        text,
+                class               text,
+                subclass            text,
+                categoria           text,
+                layer               text,
+                "rank"              int,
+                icgc_zoom           int,
+                classicgc           text,
+                icgc_id_match       bigint
             )
 AS
 $$
 SELECT 
-       -- icgc POI 
+       -- POI
        icgc_id,
        geometry,
        name,
@@ -28,12 +30,32 @@ SELECT
        categoria,
        layer,
        rank,
-       zoom AS icgc_zoom
+       zoom AS icgc_zoom,
+       classicgc,
+       icgc_id_match
 FROM contextmaps.poi
 WHERE geometry && bbox
     AND zoom_level >= zoom
     AND zoom <> 0
-;
+UNION ALL
+
+SELECT
+       -- stop_bus
+       icgc_id,
+       geometry,
+       name,
+       name AS "name:latin",
+       class,
+       NULL::text AS subclass,
+       NULL::text AS categoria,
+       NULL::text AS layer,
+       NULL::int AS "rank",
+       NULL::int AS icgc_zoom,
+       'parada_autobus' AS classicgc,
+       NULL::int AS icgc_id_match
+FROM contextmaps.stop_bus
+WHERE geometry && bbox
+    AND zoom_level >= 132;
 $$ LANGUAGE SQL STABLE
                 PARALLEL SAFE;
 -- TODO: Check if the above can be made STRICT -- i.e. if pixel_width could be NULL

6806bfde905158a2ca3c4e6aa610eb71ff4bd2e2
Fran Martin <francisco.martin@icgc.cat>
2022-05-13 10:58:03 +0000

    feat(admpt): add layer transportation

---
+++ layers/transportation/transportation.sql
@@ -0,0 +1,24 @@
+-- etldoc: layer_transportation[shape=record fillcolor=lightpink, style="rounded,filled",
+-- etldoc: label="<sql> layer_transportation |<z4> z4 |<z5> z5 |<z6> z6 |<z7> z7 |<z8> z8 |<z9> z9 |<z10> z10 |<z11> z11 |<z12> z12|<z13> z13|<z14_> z14+" ] ;
+DROP FUNCTION IF EXISTS layer_transportation(bbox geometry, zoom_level int);
+CREATE OR REPLACE FUNCTION layer_transportation(bbox geometry, zoom_level int)
+    RETURNS TABLE
+            (
+                icgc_id         int,
+                geometry        geometry,
+                class           text,
+                classicgc       text,
+                d_categori      text
+            )
+AS
+$$
+SELECT id AS icgc_id,
+       geom AS geometry,
+       class,
+       classicgc,
+       d_categori
+FROM admpt.transportation_path_tourist
+WHERE geom && bbox;
+$$ LANGUAGE SQL STABLE
+                -- STRICT
+                PARALLEL SAFE;
